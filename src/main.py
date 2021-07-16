fa7e6d87c94aa3d023ce0037c697031b0b63e6a0
njanirudh <anijaya9@gmail.com>
2021-07-16 13:40:26 +0200

    Prototype inference works

--- src/main.py
+++ src/main.py
@@ -3,62 +3,39 @@ import torch
 
 from src.segmentation_trainer import SegmentationModule
 
+if __name__ == "__main__":
+    # # -------------------------------------------------------
+    # # --------------- TRAINING CODE -------------------------
+    # # -------------------------------------------------------
+    DATASET_PATH = "/home/anirudh/NJ/Interview/Vision-Impulse/Dataset/"
+    # seg_trainer = SegmentationModule(dataset_path=DATASET_PATH,
+    #                                  in_channels=12,
+    #                                  out_channels=3,
+    #                                  batch_size=20,
+    #                                  use_rgb=False,
+    #                                  train_mode=True,
+    #                                  epochs=500
+    #                                  )
+    # seg_trainer.train_model()
 
-def get_segmentation(input_img: np.array,
-                     model_path: str,
-                     in_channels=12,
-                     out_channels=3,
-                     batch_size=20
-                     ):
-    ae_inference = SegmentationModule(in_channels=in_channels,
-                                      out_channels=out_channels,
-                                      batch_size=batch_size,
-                                      dataset_path="")
+    # -------------------------------------------------------
+    # ---------------- INFERENCE CODE -----------------------
+    # -------------------------------------------------------
+    input_img = torch.rand((10, 12, 64, 64))
+    MODEL_CHKP_PATH = "/home/anirudh/NJ/Interview/Vision-Impulse/Vision-Impulse-Test/src/lightning_logs/version_0/checkpoints/epoch=36-step=9619.ckpt"
+    seg_inference = SegmentationModule(dataset_path=DATASET_PATH,
+                                       in_channels=12,
+                                       out_channels=3,
+                                       batch_size=20,
+                                       use_rgb=False,
+                                       train_mode=False
+                                       )
 
-    # Skips during testing
-    if model_path:
-        ae_inference = SegmentationModule.load_from_checkpoint(model_path)
+    # seg_inference = SegmentationModule.load_from_checkpoint(MODEL_CHKP_PATH)
+    seg_inference.load_state_dict(torch.load(MODEL_CHKP_PATH), strict=False)
 
-    ae_inference.eval()
+    seg_inference.eval()
 
     with torch.no_grad():
-        output_seg = ae_inference(input_img)
-
-    return output_seg
-
-
-if __name__ == "__main__":
-    # -------------------------------------------------------
-    # --------------- TRAINING CODE -------------------------
-    # -------------------------------------------------------
-    DATASET_PATH = "/home/anirudh/NJ/Interview/Vision-Impulse/Dataset/"
-    seg_trainer = SegmentationModule(dataset_path=DATASET_PATH,
-                                     in_channels=12,
-                                     out_channels=3,
-                                     batch_size=20,
-                                     use_rgb=False,
-                                     train_mode=True,
-                                     epochs=500
-                                     )
-    seg_trainer.train_model()
-
-    # # -------------------------------------------------------
-    # # ---------------- INFERENCE CODE -----------------------
-    # # -------------------------------------------------------
-    # input_img = None
-    # MODEL_CHKP_PATH = ""
-    # seg_inference = SegmentationModule(dataset_path=DATASET_PATH,
-    #                                    in_channels=12,
-    #                                    out_channels=3,
-    #                                    batch_size=20,
-    #                                    use_rgb=False,
-    #                                    train_mode=False
-    #                                    )
-    #
-    # if MODEL_CHKP_PATH:
-    #     seg_inference = SegmentationModule.load_from_checkpoint(MODEL_CHKP_PATH)
-    #
-    # seg_inference.eval()
-    #
-    # with torch.no_grad():
-    #     output_seg = seg_inference(input_img)
+        output_seg = seg_inference(input_img)
+        print(output_seg.shape)
